0b6ad707b190ab2aa2402bc6917cf210ca632870
赵云 <robott@163.com>
2019-06-27 21:56:21 +0800

    活动接口

--- src/main/java/com/xikou/promotion/controller/activity/PromotionActivityController.java
+++ src/main/java/com/xikou/promotion/controller/activity/PromotionActivityController.java
@@ -6,17 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import com.xikou.common.model.PaginationModel;
 import com.xikou.common.model.PaginationVo;
 import com.xikou.common.utils.EntityCopyUtils;
 import com.xikou.common.utils.VoConvertor;
-import com.xikou.promotion.api.condition.ActivityCondition;
 import com.xikou.promotion.api.condition.ActivityOptionCondition;
+import com.xikou.promotion.api.condition.PromotionActivityCondition;
 import com.xikou.promotion.api.exception.BusinessException;
-import com.xikou.promotion.api.model.ActivityModel;
-import com.xikou.promotion.api.service.activity.ActivityService;
+import com.xikou.promotion.api.model.PromotionActivityModel;
+import com.xikou.promotion.api.service.activity.PromotionActivityService;
 import com.xikou.promotion.common.ResponseVo;
-import com.xikou.promotion.vo.ActivityVo;
+import com.xikou.promotion.vo.PromotionActivityVo;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -31,35 +33,35 @@ import io.swagger.annotations.ApiOperation;
 @Api(tags = "活动管理")
 @RestController
 @RequestMapping("/promotion/activity")
-public class ActivityController {
+public class PromotionActivityController {
 
-	private static final Logger logger = LoggerFactory.getLogger(ActivityController.class);
+	private static final Logger logger = LoggerFactory.getLogger(PromotionActivityController.class);
 
 	@Autowired
-	private ActivityService activityService;
+	private PromotionActivityService promotionActivityService;
 
 	@ApiOperation(value = "活动列表", notes = "活动列表")
 	@ResponseBody
-	@RequestMapping(value = "/activitys", method = RequestMethod.GET)
-	public ResponseEntity<ResponseVo<ActivityVo>> queryActivityList(ActivityCondition condition, //
+	@RequestMapping(value = "/queryList", method = RequestMethod.GET)
+	public ResponseEntity<ResponseVo<PromotionActivityVo>> queryActivityList(PromotionActivityCondition condition, //
 		@RequestParam(defaultValue = "1") int page, @RequestParam(defaultValue = "10") int limit) {
 		if (condition == null) {
-			condition = new ActivityCondition();
+			condition = new PromotionActivityCondition();
 		}
 		logger.debug("传参错误：{}", condition);
-		PaginationModel<ActivityModel> paginationModel = activityService.queryActivity(condition, //
+		PaginationModel<PromotionActivityModel> paginationModel = promotionActivityService.queryActivity(condition, //
 			(page - 1) * limit, limit);
-		PaginationVo<ActivityVo> paginationVo = VoConvertor.convertPaginationModelToVo(paginationModel, ActivityVo.class);
+		PaginationVo<PromotionActivityVo> paginationVo = VoConvertor.convertPaginationModelToVo(paginationModel, PromotionActivityVo.class);
 		// 返回200只表示网络请求成功 只有当ResponseVo的code为1时才代表接口响应返回成功!
-		return new ResponseEntity<ResponseVo<ActivityVo>>(ResponseVo.success("查询成功", paginationVo), HttpStatus.OK);
+		return new ResponseEntity<ResponseVo<PromotionActivityVo>>(ResponseVo.success("查询成功", paginationVo), HttpStatus.OK);
 	}
 
 	@ApiOperation(value = "新增活动", notes = "新增活动")
 	@ResponseBody
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	public ResponseEntity<ResponseVo> saveActivity(@RequestBody ActivityVo activityVo) {
+	public ResponseEntity<ResponseVo> saveActivity(@RequestBody PromotionActivityVo promotionActivityVo) {
 		try {
-			activityService.saveActiveity(EntityCopyUtils.copyBean(activityVo, ActivityModel.class));
+			promotionActivityService.saveActiveity(EntityCopyUtils.copyBean(promotionActivityVo, PromotionActivityModel.class));
 		} catch (BusinessException ex) {
 			return new ResponseEntity<>(ResponseVo.unsuccess(ex.getMessage()), HttpStatus.OK);
 		}
@@ -69,9 +71,9 @@ public class ActivityController {
 	@ApiOperation(value = "修改活动", notes = "修改活动")
 	@ResponseBody
 	@RequestMapping(value = "/modifyActivity", method = RequestMethod.POST)
-	public ResponseEntity<ResponseVo> modifyActivity(@RequestBody ActivityVo activityVo) {
+	public ResponseEntity<ResponseVo> modifyActivity(@RequestBody PromotionActivityVo promotionActivityVo) {
 		try {
-			activityService.modifyActivity(EntityCopyUtils.copyBean(activityVo, ActivityModel.class));
+			promotionActivityService.modifyActivity(EntityCopyUtils.copyBean(promotionActivityVo, PromotionActivityModel.class));
 		} catch (BusinessException ex) {
 			return new ResponseEntity<>(ResponseVo.unsuccess(ex.getMessage()), HttpStatus.OK);
 		}
@@ -81,9 +83,9 @@ public class ActivityController {
 	@ApiOperation(value = "查看活动", notes = "查看活动")
 	@ResponseBody
 	@RequestMapping(value = "/queryActivity/{activityId}", method = RequestMethod.GET)
-	public ResponseEntity<ResponseVo<ActivityVo>> queryActivity(@PathVariable String activityId) {
+	public ResponseEntity<ResponseVo<PromotionActivityVo>> queryActivity(@PathVariable String activityId) {
 		try {
-			ActivityVo activityVo = EntityCopyUtils.copyBean(activityService.queryActivityById(activityId), ActivityVo.class);
+			PromotionActivityVo activityVo = EntityCopyUtils.copyBean(promotionActivityService.queryActivityById(activityId), PromotionActivityVo.class);
 			return new ResponseEntity<>(ResponseVo.success("操作成功", activityVo), HttpStatus.OK);
 		} catch (BusinessException ex) {
 			return new ResponseEntity<>(ResponseVo.unsuccess(ex.getMessage()), HttpStatus.OK);
@@ -95,7 +97,7 @@ public class ActivityController {
 	@RequestMapping(value = "/startOrStopActivity", method = RequestMethod.POST)
 	public ResponseEntity<ResponseVo> startOrStopActivity(@RequestBody ActivityOptionCondition condition) {
 		try {
-			activityService.startOrStopActivity(condition);
+			promotionActivityService.startOrStopActivity(condition);
 			return new ResponseEntity<>(ResponseVo.success("操作成功", ""), HttpStatus.OK);
 		} catch (BusinessException ex) {
 			return new ResponseEntity<>(ResponseVo.unsuccess(ex.getMessage()), HttpStatus.OK);
---
+++ src/main/java/com/xikou/promotion/vo/PromotionActivityVo.java
@@ -0,0 +1,84 @@
+package com.xikou.promotion.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 描述：
+ * <p>
+ *
+ * @Author Yun zhao
+ * @createby ${date}
+ */
+@Data
+@ToString
+@ApiModel("活动对象")
+public class PromotionActivityVo implements Serializable {
+	@ApiModelProperty(value = "活动ID")
+	private String id;
+	@ApiModelProperty(value = "1:买一赠二(吾G)，2: 全球买手, 3：0元竞拍 4:多买多折，5:砍价得红包，6:定制拼团")
+	private Byte activityType;
+	@ApiModelProperty(value = "活动名称")
+	private String name;
+	@ApiModelProperty(value = "宣传描述1")
+	private String propagandaDesc1;
+	@ApiModelProperty(value = "宣传描述2")
+	private String propagandaDesc2;
+	@ApiModelProperty(value = "活动描述")
+	private String desc;
+	@ApiModelProperty(value = "活动参与者范围(1: 批发供应商 2: 供应链商家 3: o2o实体商家 4: 定制拼团商家)")
+	private String participantScope;
+	@ApiModelProperty(value = "活动支持商品分类")
+	private String commodityCategory;
+	@ApiModelProperty(value = "活动类别(1: 单品促销  2: 整单促销 3: 组合促销 )")
+	private String categoryRef;
+	@ApiModelProperty(value = "推广平台(1: APP 2:公众号 3: 小程序  4：H5)")
+	private String platformRef;
+	@ApiModelProperty(value = "适用用户范围(1: 用户 2: 商家)")
+	private String userScope;
+	@ApiModelProperty(value = "适用用户类型")
+	private String userTypeRef;
+	@ApiModelProperty(value = "活动限定支付类型")
+	private String payType;
+	@ApiModelProperty(value = "商品参与数量限制(不限制时为-1)")
+	private Integer commodityQuantity;
+	@ApiModelProperty(value = "活动banner图片地址")
+	private String bannerUrl;
+	@ApiModelProperty(value = "邮费方式（1、包邮 2.固定邮费 3.引用邮费模版）")
+	private Byte postageWay;
+	@ApiModelProperty(value = "邮费值")
+	private Integer postage;
+	@ApiModelProperty(value = "引用邮费模板id")
+	private String postageRef;
+	@ApiModelProperty(value = "活动是否预热(1: 不预热 2: 预热)")
+	private Byte isPreheat;
+	@ApiModelProperty(value = "活动预热提前时长 单位（天）")
+	private Integer preheatTime;
+	@ApiModelProperty(value = "活动开始时间")
+	private Date startTime;
+	@ApiModelProperty(value = "活动结束时间")
+	private Date endTime;
+	@ApiModelProperty(value = "优先级")
+	private Integer priority;
+	@ApiModelProperty(value = "开关状态 1: 开启  2:关闭")
+	private Byte switchState;
+	@ApiModelProperty(value = "活动备注")
+	private String remark;
+	@ApiModelProperty(value = "是否支持寄卖(1: 支持 2: 不支持)")
+	private Integer supportConsignment;
+	@ApiModelProperty(value = "设置最大允许寄卖时长(10天)")
+	private Integer maxConsignmentDuration;
+	@ApiModelProperty(value = "活动状态 1: 未开始  2:预热中 3: 活动中 4: 已失效")
+	private Byte state;
+	@ApiModelProperty(value = "逻辑删除字段  (1: 未删除 2: 已删除)")
+	private Byte isDeleted;
+	@ApiModelProperty(value = "创建时间")
+	private Date createTime;
+	@ApiModelProperty(value = "修改时间")
+	private Date updateTime;
+}
